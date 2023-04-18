a642abddc1b9794ec2408a54e3dbc44be8766356
Soundwave-42 <113074212+Soundwave-42@users.noreply.github.com>
2023-04-18 17:56:29 -0400

    Improved Commenting + Class structures
    Made comments in outputSubscriber more readable and added class structures.

--- src/main/java/cs505finaltemplate/CEP/OutputSubscriber.java
+++ src/main/java/cs505finaltemplate/CEP/OutputSubscriber.java
@@ -20,8 +20,8 @@ public class OutputSubscriber implements InMemoryBroker.Subscriber {
 
     @Override
     public void onMessage(Object msg) {
-
         try {
+            //Create list for holding alerted zipcodes
             List<Integer> alertZipList = new ArrayList<>();
 
             System.out.println("OUTPUT CEP EVENT: " + msg);
@@ -31,42 +31,38 @@ public class OutputSubscriber implements InMemoryBroker.Subscriber {
             //but this sticks the last output value in main
             Launcher.lastCEPOutput = String.valueOf(msg);
 
-            //Parse Event Message
+            //Parse Event Message and split into list
             String parsedMsg = String.valueOf(msg).replaceAll("\\[|\\{|}|]|\"event\":", "");
             parsedMsg = parsedMsg.replaceAll("\"zip_code\":|\"count\":|\"", "");
             String[] splitMsg = parsedMsg.split(",");
 
             //Build Alert String for each zip
             for (int i=0; i < splitMsg.length-1; i+=2){
+                separate zip and count from split message into separate variables
                 Integer zip = Integer.parseInt(splitMsg[i]);
                 Integer newCount = Integer.parseInt(splitMsg[i+1]);
 
                 //Check if zip was in previous event
                 if (prevZipAlert.containsKey(zip)) {
-                    //Check if zip's count has doubled in size
+                    //If zipcode's count has doubled in size, add zip to alert list
                     if (prevZipAlert.get(zip) * 2 <= newCount) {
-                        //Add zip to alert list
-                        alertZipList.add(zip);
+                        alertZipList.add(zip); 
                     }
-
                     //Set prevZipAlert to current event info
                     prevZipAlert.replace(zip, newCount);
                 }
                 else {
-                    //Wasn't in the last event. Add it to the list.
+                    //If zipcode wasn't in the last event. Add it to the list.
                     prevZipAlert.put(zip, newCount);
                 }
             }
-
-            //Send alert list to API
-            API.numAlertedZips = alertZipList.size();
+            //Send alert list and count to API for RTR functions
             API.alertZipList = alertZipList.stream().mapToInt(Integer::intValue).toArray();
-
+            API.numAlertedZips = alertZipList.size();
 
         } catch(Exception ex) {
             ex.printStackTrace();
         }
-
     }
 
     @Override
